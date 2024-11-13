32216eebb1bea53e7b8c1462ecf696588993c6ea
SamkovLuka <staddycase@gmail.com>
2024-11-13 17:04:25 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+
+// zd1
+
+// let name = prompt("Enter your name: ");
+// alert("Привіт, " + name);
+
+
+
+
+
+
+
+
+
+
+
+// zd2
+
+// let birthYear = Number(prompt("Enter year of your birth: "));
+// const CurrentYear = 2024;
+// age = CurrentYear-birthYear;
+// alert("Your age is " + age);
+
+
+
+
+
+
+
+
+
+// zd3
+
+// let a = Number(prompt("Enter length of square's side: "));
+// perimetr = a*4;
+// alert("Perimetr of square is " + perimetr + "cm");
+
+
+
+
+
+// zd4
+
+// let r = Number(prompt("Enter radius of circle: "));
+// square = Math.PI*r**2;
+// alert("Square of circle is " + square + "cm2");
+
+
+
+
+
+// zd5
+
+// let distance = Number(prompt("Enter distance between two cities: "));
+// let time = Number(prompt("Enter time you want to spend on driving: "));
+// speed = distance/time;
+// alert("Needed speed is " + speed + "km/h")
+
+
+
+
+
+// zd6
+
+// let dollars = Number(prompt("Enter amount of dollars: "));
+// const euro = 0.93;
+// convert = dollars*euro;
+// alert("Your amount of dollars is equal to " + convert + " euros");
+
+
+
+// zd7
+
+// let memory = Number(prompt("Enter amount of memory on flash drive: "));
+// file = 820;
+// numOfFiles = memory/(820*0.001);
+// alert("Number of files is " + numOfFiles)
+
+
+
+
+// zd8
+
+// let money = parseFloat(prompt("Enter your amount of money: "));
+// let price = parseFloat(prompt("Enter price of one bar of chocolate: "));
+
+// let chocolateCount = Math.floor(money / price);
+// let change = (money % price).toFixed(2);
+
+// console.log("You can buy " + chocolateCount + "bars of chocolate");
+// console.log("Rest of money: " + change);
+
+
+
+
+
+
+// zd9
+
+// let number = parseInt(prompt("Enter number with three digits: "));
+
+// let reversedNumber = (number % 10) * 100 + (Math.floor((number % 100) / 10)) * 10 + Math.floor(number / 100);
+
+// console.log("Reversed number: " + reversedNumber);
+
+
+
+
+// zd10
+
+// let number = parseInt(prompt("Enter integer number:"));
+
+// console.log(number % 2 === 0 && "Even" || "Odd");
